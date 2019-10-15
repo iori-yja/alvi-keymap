29e1294b58b66d57a702de7605205e7c74cdbca9
Iori YONEJI <fivo.11235813@gmail.com>
2019-10-16 02:24:12 +0900

    add an install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+FILE="$(dirname $0)/symbols/alvi"
+TARGET="/usr/share/X11/xkb/symbols/alvi"
+
+sudo --version > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+	EXEC_ROOT=sudo
+else
+	EXEC_ROOT=doas
+fi
+
+if [[ -d $(dirname ${TARGET}) ]]; then
+	${EXEC_ROOT} cp ${FILE} ${TARGET}
+fi
